7d853924fa1fa1ea0ad080b955f8641f14df52b5
inCodeSpace <box4goodmail@gmail.com>
2013-12-02 20:30:16 +0300

    test3_4

--- index.php
+++ index.php
@@ -7,7 +7,10 @@
 <body>
 	<?php echo "Обновление Новый";?>
 	<br>
-	<center><h2>Мазай - Must Die</h2></center>
+	<center>
+		<h2>Мазай - Must Die</h2>
+		Просто контент
+	</center>
 </body>
 </html>
 
